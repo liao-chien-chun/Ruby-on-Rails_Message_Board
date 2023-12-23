39b568915cbb797d617446c558d1a6cd7b87a24b
Eric Liao <d1044171009@gmail.com>
2023-12-23 21:35:44 +0800

    修改message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,10 +1,14 @@
 class Message < ApplicationRecord
-  before_save :set_created_time
+  before_create :set_created_time
+  before_update :set_updated_time
 
   private
 
   def set_created_time
     self.created_time = Time.current.strftime('%Y-%m-%d %H:%M:%S')
   end
-  
+
+  def set_updated_time
+    self.updated_time = Time.current
+  end
 end
